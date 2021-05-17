7fee02fc844cbc10a774bc8a784a72b476ca66cd
Suraj Verma <surajverma@rentalcars.com>
2021-05-17 20:47:54 +0100

    render restraurants

--- public/assets/js/restaurants.js
+++ public/assets/js/restaurants.js
@@ -1,12 +1,37 @@
-const fetchData = async (url, options = {}) => {
-  const response = await fetch(url, options);
+const fetchData = async (path, options = {}) => {
+  const BASE_URL = "http://localhost:3000";
+  const response = await fetch(`${BASE_URL}${path}`, options);
   return response.json();
 };
 
-const onReady = async () => {
-  const restaurants = await fetchData("http://localhost:3000/api/restaurants");
+const renderRestaurants = (restaurants) => {
+  const createAndAppendRestaurantCard = (restaurant) => {
+    const card = `<div class="card text-center just-ate-card">
+      <img
+        src="${restaurant.logo}"
+        class="card-img-top"
+        alt="${restaurant.name}"
+      />
+      <div class="card-body">
+        <h3 class="card-title">${restaurant.name}</h3>
+        <h5>${restaurant.type}</h5>
+        <p class="text-muted">
+          ${restaurant.address}
+        </p>
+        <a href="/restaurants/${restaurant.id}" class="btn btn-primary">Edit</a>
+      </div>
+    </div>`;
 
-  console.log(restaurants);
+    $("#restaurants").append(card);
+  };
+
+  Object.values(restaurants).forEach(createAndAppendRestaurantCard);
+};
+
+const onReady = async () => {
+  const restaurants = await fetchData("/api/restaurants");
+
+  renderRestaurants(restaurants);
 };
 
 $(document).ready(onReady);
